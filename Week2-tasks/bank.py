5e32e476b017ced828d330df9507c16a01dfaebc
unknown <michaelcurley1@gmail.com>
2024-02-01 05:32:57 +0000

    Final bank.py programme

--- Week2-tasks/bank.py
+++ Week2-tasks/bank.py
@@ -5,9 +5,10 @@
 a = int(input("Please enter amount in cents: "))
 b = int(input("Please enter amoutn in cents: "))
 result = float((a + b)/100)
+answer = format(result,".2f")
 
 '''
 print(result)
 '''
 
-print(f"The sum of these is £{result}")
+print(f"The sum of these is £{answer}")

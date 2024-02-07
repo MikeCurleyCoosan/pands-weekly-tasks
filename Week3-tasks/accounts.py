1bfd1140af88ae7976add1b22cda7754adcf9cf1
Mike_Curley <michaelcurley1@gmail.com>
2024-02-07 10:11:22 +0000

    Modified Program

--- Week3-tasks/accounts.py
+++ Week3-tasks/accounts.py
@@ -1,32 +1,38 @@
 #Question: Write a python program called accounts.py that reads in a 10 character number and outputs
 #the account number with only the last 4 digits showing (and the first 6 digits replaced with Xs)
 
+#Modify the program to deal with account numbers of any length (comment your assumptions)
+#Assumptions: I have assumed that the account number is at least 10 digits long and the last 4 digits are alwasys shown
+
 #Author: Michael Curley
 
 #Read in the account number
-accountNumber = input("Please enter a 10 digit account number: ")
+accountNumber = input("Please enter your account number: ")
 #Check what type the input is
-print(type(accountNumber))
-#Convert input to String to find length
-asString = str(accountNumber)
-print(asString)
+isInt = type(accountNumber)
 
-#Find the length of a string
-lengthOfInput = len(asString)
-print(lengthOfInput)
+#As long as an integer is entered
+if isInt == int:
+    asString = str(accountNumber) #Convert the integer to a string
+    lengthOfInput = len(asString) #And get the length of the string
+    """print(lengthOfInput)""" #testing 
+elif isInt != int: #If it isnt an integer, ask again for a 10 digit number
+    accountNumber = input("Please enter your account number: ")
+    isInt = type(accountNumber)
 
-#While the string is less than 10 characters long, ask user for 10 digit number
-while  lengthOfInput <10:
+#While the string is less than 10 digits long, ask user for 10 digit number
+while lengthOfInput <10:
     print("That account number is less than 10 numbers")
-    accountNumber = input("Please enter a 10 digit account number: ")
+    accountNumber = input("Please enter your account number: ")
     asString = str(accountNumber)
     lengthOfInput = len(asString)
-    break
+    
 
 #Get the last 4 characters of the string version of input
-slicedString = asString[6:]
+slicedString = asString[lengthOfInput-4:]
 
-hideFront = "xxxxxx"
+#Put an x for all charactes apart from the last 4 charactes
+hideFront = "x" * (lengthOfInput-4)
 result = hideFront + slicedString
 print(result)
 

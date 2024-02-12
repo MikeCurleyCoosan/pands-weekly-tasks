c15d879a51250d4d65b6c921026df989cd64dd16
Mike_Curley <michaelcurley1@gmail.com>
2024-02-12 17:00:55 +0000

    Adding comments to collatz.py

--- Week4-tasks/collatz.py
+++ Week4-tasks/collatz.py
@@ -2,20 +2,20 @@
 #of the following calculation. At each step calculate the next value by taking the current value and, if it is even,
 #divide it by two, but if it is odd, multiply it by three and add one. Have the program end if the current value is one.
 
-number = int(input('Please enter a positive number: '))
+number = int(input('Please enter a positive number: ')) #Ask user for positive number 
 
-if number < 0:
-    print('That is not a positive number, Please try again: ')
-    number = int(input('Please enter a positive number: '))
+if number < 0: #If number given is not positive
+    print('That is not a positive number, Please try again: ') #Tell the user the number entered is not positive
+    number = int(input('Please enter a positive number: ')) # And ask again for positive number
 
-print(number)
+print(number) #Print the first collatz series number
 
-while number !=1:
-    if (number % 2) == 0:
-        number = int(number/2)
-        print(number)
+while number !=1: #The series ends when it reaches 1
+    if (number % 2) == 0: #Check if number is even
+        number = int(number/2) #If even divide by two and reassign to number
+        print(number) #Print out next value in series
     else:
-        number = (int(number *3)+1)
-        print(number)
+        number = (int(number *3)+1) #If number is not even, then its odd. Multiply it by 3 and add 1 to it.
+        print(number) #Print out next value in series
 
 print('You have reached one, that is the end of the series. Thank you for playing')

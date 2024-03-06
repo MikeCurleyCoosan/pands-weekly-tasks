2c5ed32e7074d0d9795e97c8bf83c4db6cdcd3ef
Mike_Curley <michaelcurley1@gmail.com>
2024-03-06 20:11:50 +0000

    Allowing for capital E's in es.py

--- Week7-tasks/es.py
+++ Week7-tasks/es.py
@@ -9,22 +9,22 @@
 import sys #import sys module. This module provides access to some variables used or maintained by the interpreter
             #and to some other functions that interact strongly with the interpreter
 
-try:
+try: #try to get the filename from the command line, if it is not there, print an error message and exit the program
     FILENAME = sys.argv[1] #sys.argv is a list in Python, which contains the command-line arguments passed to the script.
-                        #argv[1] is the argument passed to the script. In this case, it is the filename of the text file
-except:
+                            #argv[1] is the argument passed to the script. In this case, it is the filename of the text file
+except: #if the filename is not passed as an argument, print an error message and exit the program
     print("Please run the program with a filename as an argument")
     sys.exit()
 
 try:
     with open(FILENAME, "r") as f: #open the file in read mode
         text = f.read() #read the file and store the contents in the variable text
-        count = text.count("e") #count the number of 'e's in the text file
+        count = text.count("e") + text.count("E")  #count the number of 'e's in the text file
         print(f"The file {FILENAME} contains {count} 'e's") #print the number of 'e's in the file
-except FileNotFoundError:
+except FileNotFoundError:   #catch the FileNotFoundError exception which occurs when the file is not found
     print(f"The file {FILENAME} was not found")
     sys.exit()
-except NotAFilenameError:
-    print(f"The file {FILENAME} is not a valid filename")
+except:
+    print("An error occurred while reading the file") #catch any other errors that may occur
     sys.exit()
 

f717bdb70592e83d27b33d5b344e70f556e688ec
Michael Curley <michaelcurley1@gmail.com>
2024-03-06 23:45:21 +0000

    tidying up weekday.py

--- Week5-tasks/Weekday.py
+++ Week5-tasks/Weekday.py
@@ -4,10 +4,10 @@
 import datetime #The module datetime provides classed for manipulating dates and times
 
 #Get the current date
-currentDate = datetime.datetime.now() #datetime.datetime.now() returns the current date and time
+current_date = datetime.datetime.now() #datetime.datetime.now() returns the current date and time
 
 #Get the current day of the week strftime() method returns a string representing using date, time or datetime object
-day = currentDate.strftime("%A") #%a Abbreviated weekday name, %A Full weekday name, %w Weekday as a decimal number, 
+day = current_date.strftime("%A") #%a Abbreviated weekday name, %A Full weekday name, %w Weekday as a decimal number, 
 print(day)
 
 #If the day is a weekday, print that it is a weekday

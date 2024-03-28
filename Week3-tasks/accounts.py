59d7e961d8e5afb76bed3b76926a21103b5b8b7b
Mike_Curley <michaelcurley1@gmail.com>
2024-03-28 09:49:01 +0000

    Reworked accounts.py to account for string & floats being entered instead of integers

--- Week3-tasks/accounts.py
+++ Week3-tasks/accounts.py
@@ -7,33 +7,46 @@
 
 #Author: Michael Curley
 
-#Read in the account number
-accountNumber = int(input("Please enter a 10 digit account number: "))
-#Check what type the input is
-isInt = type(accountNumber)
+length_of_input = 0
 
-#As long as an integer is entered
-if isInt == int:
-    asString = str(accountNumber) #Convert the integer to a string
-    lengthOfInput = len(asString) #And get the length of the string
-    """print(lengthofinput)""" #testing 
-elif isInt != int: #If it isnt an integer, ask again for a 10 digit number
-    accountNumber = int(input("Please enter your account number: "))
-    isInt = type(accountNumber)
+#Run the program until the user enters a 10 digit account number
+while True and length_of_input < 10:
+    #Ask the user for a 10 digit account number
+    account_number = input("Please enter a valid 10 digit accound number ")
+    #Check if the input is an integer
+    try:
+        #If the input is an integer, convert it to a string and get the length of the string
+        number_as_int = int(account_number)
+        number_as_str = str(account_number)
+        length_of_input = len(number_as_str)
+        #If the length of the input is less than 10, ask the user for a 10 digit account number
+        if length_of_input < 10:
+            print("The account number is less than 10 digits long, please enter a valid 10 digit account number")
+            continue
 
-#While the string is less than 10 digits long, ask user for 10 digit number
-while lengthOfInput <10:
-    print("That account number is less than 10 numbers")
-    accountNumber = int(input("Please enter your account number: "))
-    asString = str(accountNumber)
-    lengthOfInput = len(asString)
-    
+        else:
+            #Get the last 4 characters of the string version of input
+            sliced_string = number_as_str[length_of_input-4:]
+            #Put an x for all charactes apart from the last 4 charactes
+            hide_front = "x" * (length_of_input-4)
+            #Combine the x's and the last 4 characters
+            result = hide_front + sliced_string
+            #Print the result
+            print(result)
+        break;
+    #If the input is not an integer, ask the user for a valid number
+    except ValueError:
+        try:
+            #If the input is a float, ask the user for a valid number
+            val = float(account_number)
+            print("Input is an float number, account numbers should contain integers only.")
+            continue
+        except ValueError:
+            #If the input is a string, ask the user for a valid number
+            print("This is not a number, its a string. Please enter a valid 10 digit account number")
+            continue
 
-#Get the last 4 characters of the string version of input
-slicedString = asString[lengthOfInput-4:]
-
-#Put an x for all charactes apart from the last 4 charactes
-hideFront = "x" * (lengthOfInput-4)
-result = hideFront + slicedString
-print(result)
-
+#Problems encountered while writing the code:
+        # Leading zeros were being removed from the account number when the user entered a number with leading zeros
+        # The program was not checking if the input was a float or a string
+        # The program was not checking if the input was less than 10 digits long
